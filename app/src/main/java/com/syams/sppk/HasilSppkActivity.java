f43ddc2d3a4e6d866349b4212992790cc676b073
syams <syamsrachman@gmail.com>
2018-06-03 13:57:09 +0700

    sppk

--- app/src/main/java/com/syams/sppk/HasilSppkActivity.java
+++ app/src/main/java/com/syams/sppk/HasilSppkActivity.java
@@ -2,6 +2,7 @@ package com.syams.sppk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HasilSppkActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class HasilSppkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_sppk);
+
+
+        TextView txt_hasil = (TextView) findViewById(R.id.txt_hasil);
+        txt_hasil.setText(getIntent().getStringExtra("hasil"));
     }
 }
